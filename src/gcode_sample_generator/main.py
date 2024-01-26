4a2e57fb59c1a906aea0109fee8d6ae01bd4481a
Riley Hutchinson <rileyhutchinson18@gmail.com>
2024-01-25 20:49:59 -0800

    Adds reading in the cutting tools json

--- src/gcode_sample_generator/main.py
+++ src/gcode_sample_generator/main.py
@@ -1,4 +1,23 @@
+import os
+import json
+import random
 
+def load_sample_tools_json():
+    file_path = os.path.join(os.path.dirname(__file__), '..', 'data', 'cuttingtools', 'sample_tools.json')
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+        return data
+    
+def select_random_keys(json_data, num_keys=3):
+    keys_list = list(json_data.keys())
+    keys = random.sample(keys_list, min(num_keys, len(keys_list)))
+    selected_data = {key: json_data[key] for key in keys}
+    return selected_data
+
+def run():
+    cutting_tools = select_random_keys(load_sample_tools_json())
+
+    return None
 
 if __name__ == "__main__":
-    print("Hello World!")
+    run()
